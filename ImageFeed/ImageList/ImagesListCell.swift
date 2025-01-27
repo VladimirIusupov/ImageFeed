eeac70fd5d5f787bcab535e558c411026440f38d
Vladimir Iusupov <vn.yusupov@gmail.com>
2025-01-27 10:54:53 +0300

    Bug fixes + adding fron review

--- ImageFeed/ImageList/ImagesListCell.swift
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -1,8 +1,9 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+    // MARK: - Static properties
     static let reuseIdentifier = "ImagesListCell"
-    
+    // MARK: - Dinamic properties
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var dateLabel: UILabel!
--- ImageFeed/ImageList/ImagesListViewController.swift
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -1,26 +1,25 @@
 import UIKit
 
 final class ImagesListViewController: UIViewController {
-    
+    // MARK: - IBOutlet properties
     @IBOutlet weak var tableView: UITableView!
-    
+    // MARK: - Static properties
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
-    
+    private let currentDate = Date()
+    // MARK: - Dinamic propeties
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
         formatter.timeStyle = .none
         return formatter
     }()
-    
+    // MARK: - method didload
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
 }
-
+    // MARK: - extension properties
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return photosName.count
@@ -40,13 +39,13 @@ extension ImagesListViewController: UITableViewDataSource {
 }
 
 extension ImagesListViewController {
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+    private func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
             return
         }
 
         cell.cellImage.image = image
-        cell.dateLabel.text = dateFormatter.string(from: Date())
+        cell.dateLabel.text = dateFormatter.string(from: currentDate)
 
         let isLiked = indexPath.row % 2 == 0
         let likeImage = isLiked ? UIImage(named: "likebutton_on") : UIImage(named: "likebutton_off")
