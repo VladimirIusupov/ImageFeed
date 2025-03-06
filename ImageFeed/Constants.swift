382cd532805f39089c595652f9f3a2a106a97551
Vladimir Iusupov <vn.yusupov@gmail.com>
2025-03-06 12:42:39 +0300

    first changes from sprint 10

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+enum Constants {
+    static let accessKey = "OJcrfLnPv4Wxxd8lEFsrcRD2gRxacXqZVnOBio1nJlg"
+    static let secretKey = "cxypN_OSFP4gTydwoNpvh1DQ7ipVzKaEaxIMEZZ0Nh8"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")
+}
